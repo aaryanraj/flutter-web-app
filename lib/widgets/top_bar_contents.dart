a49e12f71875589830512ccc8d21aeb64f002f3b
aaryanraj <aaryanraj5314@gmail.com>
2023-01-15 16:16:10 +0530

    feat: FWA-02: Navbar customization

--- lib/widgets/top_bar_contents.dart
+++ lib/widgets/top_bar_contents.dart
@@ -25,75 +25,280 @@ class _TopBarContentsState extends State<TopBarContents> {
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
 
-    return PreferredSize(
-      preferredSize: Size(screenSize.width, 70),
-      child: Container(
-        color: Colors.white.withOpacity(widget.opacity),
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(width: screenSize.width/4,),
-                    Text(
-                      'Author',
-                      style: TextStyle(
-                        color: Color(0xFF077bd7),
-                        fontSize: 26,
-                        fontFamily: 'Raleway',
-                        fontWeight: FontWeight.w900,
-                        letterSpacing: 3,
-                      ),
+    return Container(
+      color: Colors.white.withOpacity(widget.opacity),
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  /* SizedBox(
+                    width: screenSize.width / 4,
+                  ),*/
+                  Text(
+                    'eBookStore',
+                    style: TextStyle(
+                      color: Color(0xFF077bd7),
+                      fontSize: 26,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w900,
+                      letterSpacing: 3,
                     ),
-                    SizedBox(width: screenSize.width / 15),
-                    InkWell(
-                      onHover: (value) {
-                        setState(() {
-                          value
-                              ? _isHovering[0] = true
-                              : _isHovering[0] = false;
-                        });
-                      },
-                      onTap: () {},
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            'Home',
-                            style: TextStyle(
-                                color: _isHovering[0]
-                                    ? Color(0xFF077bd7)
-                                    : Color(0xFF077bd7),
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16
-                            ),
+                  ),
+                  SizedBox(width: screenSize.width / 3),
+                  InkWell(
+                    onHover: (value) {
+                      setState(() {
+                        value ? _isHovering[0] = true : _isHovering[0] = false;
+                      });
+                    },
+                    onTap: () {},
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Home',
+                          style: TextStyle(
+                              color: _isHovering[0]
+                                  ? Color(0xFF077bd7)
+                                  : Color(0xFF077bd7),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16),
+                        ),
+                        SizedBox(height: 5),
+                        Visibility(
+                          maintainAnimation: true,
+                          maintainState: true,
+                          maintainSize: true,
+                          visible: _isHovering[0],
+                          child: Container(
+                            height: 2,
+                            width: 20,
+                            color: Color.fromARGB(255, 102, 134, 231),
                           ),
-                          SizedBox(height: 5),
-                          Visibility(
-                            maintainAnimation: true,
-                            maintainState: true,
-                            maintainSize: true,
-                            visible: _isHovering[0],
-                            child: Container(
-                              height: 2,
-                              width: 20,
-                              color: Color(0xFF051441),
-                            ),
-                          )
-                        ],
-                      ),
+                        )
+                      ],
                     ),
-
-                  ],
-                ),
+                  ),
+                  SizedBox(width: screenSize.width / 30),
+                  InkWell(
+                    onHover: (value) {
+                      setState(() {
+                        value ? _isHovering[1] = true : _isHovering[1] = false;
+                      });
+                    },
+                    onTap: () {},
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'About',
+                          style: TextStyle(
+                              color: _isHovering[0]
+                                  ? Color(0xFF077bd7)
+                                  : Color(0xFF077bd7),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16),
+                        ),
+                        SizedBox(height: 5),
+                        Visibility(
+                          maintainAnimation: true,
+                          maintainState: true,
+                          maintainSize: true,
+                          visible: _isHovering[1],
+                          child: Container(
+                            height: 2,
+                            width: 20,
+                            color: Color.fromARGB(255, 102, 134, 231),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: screenSize.width / 30),
+                  InkWell(
+                    onHover: (value) {
+                      setState(() {
+                        value ? _isHovering[2] = true : _isHovering[2] = false;
+                      });
+                    },
+                    onTap: () {},
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Books',
+                          style: TextStyle(
+                              color: _isHovering[0]
+                                  ? Color(0xFF077bd7)
+                                  : Color(0xFF077bd7),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16),
+                        ),
+                        SizedBox(height: 5),
+                        Visibility(
+                          maintainAnimation: true,
+                          maintainState: true,
+                          maintainSize: true,
+                          visible: _isHovering[2],
+                          child: Container(
+                            height: 2,
+                            width: 20,
+                            color: Color.fromARGB(255, 102, 134, 231),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: screenSize.width / 30),
+                  InkWell(
+                    onHover: (value) {
+                      setState(() {
+                        value ? _isHovering[3] = true : _isHovering[3] = false;
+                      });
+                    },
+                    onTap: () {},
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Search',
+                          style: TextStyle(
+                              color: _isHovering[0]
+                                  ? Color(0xFF077bd7)
+                                  : Color(0xFF077bd7),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16),
+                        ),
+                        SizedBox(height: 5),
+                        Visibility(
+                          maintainAnimation: true,
+                          maintainState: true,
+                          maintainSize: true,
+                          visible: _isHovering[3],
+                          child: Container(
+                            height: 2,
+                            width: 20,
+                            color: Color.fromARGB(255, 102, 134, 231),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: screenSize.width / 30),
+                  InkWell(
+                    onHover: (value) {
+                      setState(() {
+                        value ? _isHovering[4] = true : _isHovering[4] = false;
+                      });
+                    },
+                    onTap: () {},
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Contact',
+                          style: TextStyle(
+                              color: _isHovering[0]
+                                  ? Color(0xFF077bd7)
+                                  : Color(0xFF077bd7),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16),
+                        ),
+                        SizedBox(height: 5),
+                        Visibility(
+                          maintainAnimation: true,
+                          maintainState: true,
+                          maintainSize: true,
+                          visible: _isHovering[4],
+                          child: Container(
+                            height: 2,
+                            width: 20,
+                            color: Color.fromARGB(255, 102, 134, 231),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: screenSize.width / 30),
+                  InkWell(
+                    onHover: (value) {
+                      setState(() {
+                        value ? _isHovering[5] = true : _isHovering[5] = false;
+                      });
+                    },
+                    onTap: () {},
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Sign In',
+                          style: TextStyle(
+                              color: _isHovering[0]
+                                  ? Color(0xFF077bd7)
+                                  : Color(0xFF077bd7),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16),
+                        ),
+                        SizedBox(height: 5),
+                        Visibility(
+                          maintainAnimation: true,
+                          maintainState: true,
+                          maintainSize: true,
+                          visible: _isHovering[5],
+                          child: Container(
+                            height: 2,
+                            width: 20,
+                            color: Color.fromARGB(255, 102, 134, 231),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  //SizedBox(width: screenSize.width / 30),
+                  Padding(padding: EdgeInsets.fromLTRB(5, 0, 5, 0)),
+                  InkWell(
+                    onHover: (value) {
+                      setState(() {
+                        value ? _isHovering[6] = true : _isHovering[6] = false;
+                      });
+                    },
+                    onTap: () {},
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Sign Up',
+                          style: TextStyle(
+                              color: _isHovering[0]
+                                  ? Color(0xFF077bd7)
+                                  : Color(0xFF077bd7),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16),
+                        ),
+                        SizedBox(height: 5),
+                        Visibility(
+                          maintainAnimation: true,
+                          maintainState: true,
+                          maintainSize: true,
+                          visible: _isHovering[6],
+                          child: Container(
+                            height: 2,
+                            width: 20,
+                            color: Color.fromARGB(255, 102, 134, 231),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
-
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
